f2940544181476682941ccd5b645c403a9f092f0
Telitsyna Yulia <tuavua@gmail.com>
2023-09-29 15:21:56 +0300

    hjghjg

--- src/Tank.java
+++ src/Tank.java
@@ -60,7 +60,7 @@ public class Tank {
                 fuel=0;
                 break;
         }
-        System.out.println("топливо закончилось");
+        System.out.println("топливо ggggзакончилось");
     }
 
 }
