75c30d3f3b2b9321a99c51e5fa6d5c1d98047a27
Telitsyna Yulia <tuavua@gmail.com>
2023-10-18 22:18:13 +0300

    chess

--- src/Chess/ChessPiece.java
+++ src/Chess/ChessPiece.java
@@ -3,14 +3,14 @@ package Chess;
 import Chess.ChessBoard;
 
 abstract public class ChessPiece {
-    String color;
-    boolean chek=true;
+    public String color;
+    boolean check=true;
 public ChessPiece (String color)
 {this.color=color;}
     public String getColor()
     { return this.color;}
 // этот метод почему-то должен быть абстрактным
-    abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+     abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
 
     abstract String getSymbol();
     // какую фигуру я должна тут вернуть
